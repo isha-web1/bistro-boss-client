4be71a79af6ec6566d9179b2f0c2a9a044c2e430
isha-web1 <isha.web1@gmail.com>
2023-05-26 23:25:27 +0600

    banner added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
+import Category from '../Category/Category';
 
 const Home = () => {
     return (
         <div>
-                <h1>this is home page</h1>
+                <Banner></Banner>
+                <Category></Category>
         </div>
     );
 };
