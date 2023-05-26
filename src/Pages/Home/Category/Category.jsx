dd39cad599fac5a828d6fc8ce9c169afe7785057
isha-web1 <isha.web1@gmail.com>
2023-05-26 23:36:51 +0600

    food category added

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -3,6 +3,11 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
+import slider1 from '../../../assets/home/slide1.jpg'
+import slider2 from '../../../assets/home/slide2.jpg'
+import slider3 from '../../../assets/home/slide3.jpg'
+import slider4 from '../../../assets/home/slide4.jpg'
+import slider5 from '../../../assets/home/slide5.jpg'
 
 const Category = () => {
     return (
@@ -14,17 +19,29 @@ const Category = () => {
           clickable: true,
         }}
         modules={[Pagination]}
-        className="mySwiper"
+        className="mySwiper mb-24"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide>
+            <img src={slider1} alt="" />
+            <h1 className='text-4xl uppercase text-center -mt-16 text-white'>salad</h1>
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slider2} alt="" />
+            <h1 className='text-4xl uppercase text-center -mt-16 text-white'>pizza</h1>
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slider3} alt="" />
+            <h1 className='text-4xl uppercase text-center -mt-16 text-white'>soup</h1>
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slider4} alt="" />
+            <h1 className='text-4xl uppercase text-center -mt-16 text-white'>desert</h1>
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slider5} alt="" />
+            <h1 className='text-4xl uppercase text-center -mt-16 text-white'>salad</h1>
+        </SwiperSlide>
+        
       </Swiper>
     );
 };
