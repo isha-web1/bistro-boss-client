25a5a4c4f73746d798e985ef7493c97b9f47448a
isha-web1 <isha.web1@gmail.com>
2023-05-28 23:42:59 +0600

    different category added

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -3,6 +3,9 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import Cover from "./Cover/Cover";
 import menuCover from '../../../assets/menu/banner3.jpg'
 import dessertImg from '../../../assets/menu/dessert-bg.jpeg'
+import pizzaImg from '../../../assets/menu/pizza-bg.jpg'
+import saladImg from '../../../assets/menu/salad-bg.jpg'
+import soupImg from '../../../assets/menu/soup-bg.jpg'
 import PopularMenu from "../../Home/PopularMenu/PopularMenu";
 import useMenu from "../../../hooks/UseMenu";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
@@ -28,6 +31,12 @@ const Menu = () => {
       <MenuCategory items={offered}></MenuCategory>
       {/* dessert menu items */}
       <MenuCategory items={dessert} title={dessert} img={dessertImg}></MenuCategory>
+      {/* pizza menu items */}
+      <MenuCategory items={pizza} title={pizza} img={pizzaImg}></MenuCategory>
+      {/* salad menu items */}
+      <MenuCategory items={salad} title={salad} img={saladImg}></MenuCategory>
+      {/* soup menu items */}
+      <MenuCategory items={soup} title={soup} img={soupImg}></MenuCategory>
     </div>
   );
 };
--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import MenuItem from "../../Shared/Footer/MenuItem/MenuItem";
 import Cover from "../../Menu/Menu/Cover/Cover";
 
-const MenuCategory = ({items, title, img}) => {
+const MenuCategory = ({items,title,  img}) => {
+    
   return (
     <div className="pt-8">
-        {title && <Cover img={img} title={'our menu'}></Cover> }
+        {title && <Cover img={img} title='this is menu'></Cover> }
       <div className="grid md: grid-cols-2 gap-10 mt-16">
-        {items.map((item) => (
-          <MenuItem key={item._item} item={item}></MenuItem>
+        {items && items.map((item) => (
+          <MenuItem  item={item}></MenuItem>
         ))}
       </div>
     </div>
