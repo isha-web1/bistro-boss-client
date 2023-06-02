3a7b1d6c5430aa8d509f3cef41959530e125fed6
isha-web1 <isha.web1@gmail.com>
2023-06-03 00:16:42 +0600

    add to cart complete

--- src/Pages/Dashboard/MyCart/MyCart.jsx
+++ src/Pages/Dashboard/MyCart/MyCart.jsx
@@ -1,19 +1,68 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
 import useCart from "../../../hooks/useCart";
+import { FaTrashAlt } from "react-icons/fa";
 
 const MyCart = () => {
-    const [cart] = useCart()
-    const total = cart.reduce((sum, item) => item.price + sum, 0)
+  const [cart] = useCart();
+  const total = cart.reduce((sum, item) => item.price + sum, 0);
   return (
     <div>
       <Helmet>
         <title>Bistro Boss | my cart</title>
       </Helmet>
-      <h1>my car page</h1>
-      <div>
-        <h1 className="text-3xl" >toatla items : {cart.length}</h1>
-        <h1 className="text-3xl" >total price : {total}</h1>
+      
+      <div className="uppercase font-semibold h-[60px] flex justify-evenly items-center">
+        <h1 className="text-3xl">toatla items : {cart.length}</h1>
+        <h1 className="text-3xl">total price : {total}</h1>
+        <button className="btn btn-warning btn-sm">Pay</button>
+      </div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                #
+              </th>
+              <th>Food</th>
+              <th>Item Name</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+                cart.map((item, index)=> <tr key={item._id}>
+                    <td>
+                     {index + 1}
+                    </td>
+                    <td>
+                    <div className="avatar">
+                          <div className="mask mask-squircle w-12 h-12">
+                            <img
+                              src={item.image}
+                              alt="Avatar Tailwind CSS Component"
+                            />
+                          </div>
+                        </div>
+                    </td>
+                    <td>
+                      {item.name}
+                    </td>
+                    <td className="text-end">${item.price}</td>
+                    <td>
+                      <button className="btn btn-ghost btn-lg bg-red-600 text-white"><FaTrashAlt></FaTrashAlt></button>
+                    </td>
+                  </tr>)
+            }
+            
+            
+           
+          </tbody>
+          
+          
+        </table>
       </div>
     </div>
   );
