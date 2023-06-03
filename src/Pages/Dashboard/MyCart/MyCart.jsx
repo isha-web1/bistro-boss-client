20fda26b4098f766ec023f26dce7cb97d69490ba
isha-web1 <isha.web1@gmail.com>
2023-06-03 12:11:51 +0600

    use refetch to delete cart

--- src/Pages/Dashboard/MyCart/MyCart.jsx
+++ src/Pages/Dashboard/MyCart/MyCart.jsx
@@ -2,12 +2,43 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 import useCart from "../../../hooks/useCart";
 import { FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const MyCart = () => {
-  const [cart] = useCart();
+  const [cart, refetch] = useCart();
   const total = cart.reduce((sum, item) => item.price + sum, 0);
+
+  const handleDelete = item => {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+         fetch(`http://localhost:5000/carts/${item._id}`,{
+            method : 'DELETE'
+         })
+         .then(res => res.json())
+         .then(data =>{
+            if(data.deletedCount>0){
+                refetch();
+                Swal.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                  )
+            }
+         })
+        }
+      })
+  }
+
   return (
-    <div>
+    <div className="w-full h-full">
       <Helmet>
         <title>Bistro Boss | my cart</title>
       </Helmet>
@@ -52,7 +83,7 @@ const MyCart = () => {
                     </td>
                     <td className="text-end">${item.price}</td>
                     <td>
-                      <button className="btn btn-ghost btn-lg bg-red-600 text-white"><FaTrashAlt></FaTrashAlt></button>
+                      <button onClick={()=>handleDelete(item)} className="btn btn-ghost  bg-red-600 text-white"><FaTrashAlt></FaTrashAlt></button>
                     </td>
                   </tr>)
             }
